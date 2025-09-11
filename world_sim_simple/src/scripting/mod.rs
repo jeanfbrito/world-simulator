2b6d0effa054ab47c8699f579b783bb264e82f2e
Jean Brito <jeanfbrito@gmail.com>
2025-09-10 22:40:03 -0300

    feat: prepare GOAP actions for Lua scripting
    - Create goap_loader module for loading GOAP actions from Lua scripts
    - Add gather_berries action already defined in assets/packs/stronghold/scripts/ai/goap_actions.lua
    - Module commented out pending bevy_mod_scripting integration (uses mlua directly)
    - Foundation laid for making AI behaviors fully data-driven and moddable
    
    The Lua scripts already define gather_berries and other GOAP actions,
    ready to be loaded once the scripting integration is complete.

--- world_sim_simple/src/scripting/mod.rs
+++ world_sim_simple/src/scripting/mod.rs
@@ -5,11 +5,13 @@ use crate::debug::{DebugSystem, DebugLevel};
 pub mod lua_api;
 pub mod recipe_scripts;
 pub mod tree_generation;
+// pub mod goap_loader;  // TODO: Enable when bevy_mod_scripting integration is ready
 // pub mod storage_loader; // TODO: Enable when mlua is configured
 
 // Re-export key types for easier use
 pub use recipe_scripts::{RecipeScript, ReloadRecipeScriptsCommand};
 pub use tree_generation::{ScriptedTree, GenerateTreesCommand, TreeGenerationState};
+// pub use goap_loader::{ScriptedGoapActions, ReloadGoapActionsCommand};
 
 #[derive(Event)]
 pub struct ScriptReloadEvent {
@@ -21,6 +23,7 @@ pub enum ScriptType {
     Recipe,
     Worker,
     World,
+    Goap,
 }
 
 pub struct ScriptingPlugin;
@@ -31,13 +34,17 @@ impl Plugin for ScriptingPlugin {
             .add_event::<ScriptReloadEvent>()
             .add_event::<ReloadRecipeScriptsCommand>()
             .add_event::<GenerateTreesCommand>()
+            // .add_event::<ReloadGoapActionsCommand>()  // TODO: Enable when ready
             .init_resource::<TreeGenerationState>()
+            // .init_resource::<ScriptedGoapActions>()  // TODO: Enable when ready
             .add_systems(Startup, scripting_init_system)
             .add_systems(Update, (
                 script_reload_system,
                 recipe_scripts::load_recipe_scripts,
                 recipe_scripts::process_recipe_scripts,
                 tree_generation::generate_trees_system,
+                // goap_loader::load_goap_actions_system,  // TODO: Enable when ready
+                // goap_loader::merge_goap_actions_system,  // TODO: Enable when ready
             ));
     }
 }
@@ -46,6 +53,7 @@ fn scripting_init_system(
     debug: Res<DebugSystem>,
     mut reload_events: EventWriter<ReloadRecipeScriptsCommand>,
     mut tree_events: EventWriter<GenerateTreesCommand>,
+    // mut goap_events: EventWriter<ReloadGoapActionsCommand>,  // TODO: Enable when ready
 ) {
     debug.log(
         DebugLevel::Info,
@@ -73,11 +81,23 @@ fn scripting_init_system(
         "SCRIPT", 
         "Triggering initial tree generation"
     );
+    
+    // TODO: Load GOAP actions when ready
+    // goap_events.send(ReloadGoapActionsCommand {
+    //     pack_name: Some("stronghold".to_string()),
+    // });
+    
+    // debug.log(
+    //     DebugLevel::Info,
+    //     "SCRIPT",
+    //     "Triggering initial GOAP actions loading"
+    // );
 }
 
 fn script_reload_system(
     mut reload_events: EventReader<ScriptReloadEvent>,
     mut recipe_reload_events: EventWriter<ReloadRecipeScriptsCommand>,
+    // mut goap_reload_events: EventWriter<ReloadGoapActionsCommand>,  // TODO: Enable when ready
     debug: Res<DebugSystem>,
 ) {
     for event in reload_events.read() {
@@ -96,6 +116,17 @@ fn script_reload_system(
                     DebugLevel::Debug,
                     "SCRIPT",
                     "Worker script reloading not yet implemented"
+                );
+            }
+            ScriptType::Goap => {
+                // TODO: Enable when ready
+                // goap_reload_events.send(ReloadGoapActionsCommand {
+                //     pack_name: None,  // Use default pack
+                // });
+                debug.log(
+                    DebugLevel::Debug,
+                    "SCRIPT",
+                    "GOAP script reloading not yet implemented"
                 );
             }
             ScriptType::World => {
